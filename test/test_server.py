666268f57425a2579a237626074b90d4112c4e6e
Jaeha Choi <jaeha220@gmail.com>
2021-04-10 15:07:27 -0700

    Add raw test server

---
+++ test/test_server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = ''
+PORT = 1234
+BUFFER_SIZE = 4096
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        by = b''
+        while True:
+            data = conn.recv(BUFFER_SIZE)
+            if not data:
+                break
+            by += data
+            print(by)
