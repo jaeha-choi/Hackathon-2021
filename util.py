29917e6ba8b5ecebdcfc2ef810d99d98e87f51e2
Jaeha Choi <jaeha220@gmail.com>
2021-04-10 01:10:24 -0700

    Add comments for util.py

--- util.py
+++ util.py
@@ -3,8 +3,13 @@ import socket
 import struct
 
 
-# Return 0 for valid, 1 for invalid ip address, 2 for invalid port
 def validate(ip_addr: str, port: int):
+    """
+    Validate IP address and port
+    :param ip_addr: IP address to validate
+    :param port: Port to validate
+    :return: Return 0 for valid, 1 for invalid ip address, 2 for invalid port
+    """
     try:
         socket.inet_aton(ip_addr)
     except socket.error:
@@ -14,8 +19,34 @@ def validate(ip_addr: str, port: int):
     return 0
 
 
-# Return True if msg was sent, False if encountered an error.
+def _recv_n_byte(conn: socket.socket, packet_size: int):
+    """
+    Private function to receive n amount of bytes
+    :param conn: Connection socket
+    :param packet_size: Total size of a packet to receive
+    :return: Received data in bytes. None if not all bytes were received.
+    """
+    data = b''
+    try:
+        while len(data) < packet_size:
+            packet = conn.recv(packet_size - len(data))
+            if not packet:
+                return None
+            data += packet
+    except:
+        print("Unknown error in _recv_n_byte")
+        return None
+    return data
+
+
 def send_str(conn: socket.socket, msg: str, encoding: str = "utf-8") -> bool:
+    """
+    Send text as string format
+    :param conn: Connection socket
+    :param msg: String to send
+    :param encoding: Encoding to use for the string
+    :return: Return True if msg was sent, False otherwise.
+    """
     try:
         # This is inefficient as the size can get big very fast
         # b_msg = b'%i]' % len(b_msg) + msg.encode(encoding=encoding)
@@ -34,22 +65,13 @@ def send_str(conn: socket.socket, msg: str, encoding: str = "utf-8") -> bool:
     return True
 
 
-# Private function to receive n amount of bytes
-def _recv_n_byte(conn: socket.socket, packet_size: int):
-    data = b''
-    try:
-        while len(data) < packet_size:
-            packet = conn.recv(packet_size - len(data))
-            if not packet:
-                return None
-            data += packet
-    except:
-        print("Unknown error in _recv_n_byte")
-        return None
-    return data
-
-
 def recv_str(conn: socket.socket, encoding: str = "utf-8") -> (str, bool):
+    """
+    Receive text as string format
+    :param conn: Connection socket
+    :param encoding: Encoding to use for string
+    :return: Tuple of received string and Boolean indicating the receive status.
+    """
     string = ""
     try:
         pkg_len = _recv_n_byte(conn, 4)
@@ -71,8 +93,14 @@ def recv_str(conn: socket.socket, encoding: str = "utf-8") -> (str, bool):
     return string, True
 
 
-# Send as binary format
 def send_bin(conn: socket.socket, file_n: str, buff_size: int = 4096) -> bool:
+    """
+    Send file as binary format
+    :param conn: Connection socket
+    :param file_n: File name to save as
+    :param buff_size: Size of a buffer
+    :return: True if successfully sent, False otherwise.
+    """
     try:
         with open(file_n, "rb") as file:
             b_msg = struct.pack('!L', os.path.getsize(file_n))
@@ -92,8 +120,13 @@ def send_bin(conn: socket.socket, file_n: str, buff_size: int = 4096) -> bool:
     return True
 
 
-# Save as binary format
 def recv_bin(conn: socket.socket, file_n: str) -> bool:
+    """
+    Receive file as binary format
+    :param conn: Connection socket
+    :param file_n: File name to save as
+    :return: True if successfully received, False otherwise.
+    """
     try:
         pkg_len = _recv_n_byte(conn, 4)
         packet_size = struct.unpack('!L', pkg_len)[0]
