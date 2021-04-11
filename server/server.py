1246a4fd5f3a8febed42593cc36d4529eca8b8e1
Jaeha Choi <jaeha220@gmail.com>
2021-04-10 18:32:30 -0700

    Add exception handling

--- server/server.py
+++ server/server.py
@@ -67,7 +67,7 @@ class Server:
                         priv_addr, _ = util.recv_str(conn)
                         self.add(uid, addr[0], addr[1], priv_addr[0], priv_addr[1])
                         util.send_str(conn, ExitCode.SUCCESS)
-                        log.debug("Clients:", self.clients)
+                        log.debug("Clients:", self.clients.values())
                         log.info("ADD command done")
 
                     elif command == str(Command.EXIT):
@@ -82,7 +82,7 @@ class Server:
                             del self.clients[uid]
                         finally:
                             mutex.release()
-                        log.debug("Clients:", self.clients)
+                        log.debug("Clients:", self.clients.values())
                         log.info("EXIT command done")
                         break
 
@@ -123,6 +123,9 @@ class Server:
                             # UUID not found in dict
                             pass
                     err_cnt = 0
+            except ConnectionResetError:
+                conn.close()
+                log.warning("Closing connection closed by peer.")
 
             except Exception as err1:
                 log.error("Unknown Error at listen", err1)
