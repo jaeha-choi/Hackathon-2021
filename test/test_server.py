ba96b319785679ae33ae969127d8c2c7b0ba74a0
Jaeha Choi <jaeha220@gmail.com>
2021-04-19 21:04:41 -0700

    Change test_server.py

--- test/test_server.py
+++ test/test_server.py
@@ -5,6 +5,7 @@ PORT = 1234
 BUFFER_SIZE = 4096
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
